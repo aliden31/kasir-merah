bb9fca8832b3fe8c806e95f42b8a93f5fff661ff
Nyai Tarsih <ntarsih32@gmail.com>
2025-07-30 13:37:21 +0000

    Hubgungkan dengan database api key ini // Import the functions you need

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgLf7dfN4AIuud2IFfYFKOiqDUyPCi0nI",
+  authDomain: "kasir-5e3ad.firebaseapp.com",
+  projectId: "kasir-5e3ad",
+  storageBucket: "kasir-5e3ad.appspot.com",
+  messagingSenderId: "1022262477243",
+  appId: "1:1022262477243:web:8637d3184d242c3af3049e"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
