86ca96d267fe3b1ef9405d7a153041f59ff51075
Nyai Tarsih <ntarsih32@gmail.com>
2025-07-30 17:02:35 +0000

    Buatkan local envnya juga dan rules untuk database firestore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAgLf7dfN4AIuud2IFfYFKOiqDUyPCi0nI",
-  authDomain: "kasir-5e3ad.firebaseapp.com",
-  projectId: "kasir-5e3ad",
-  storageBucket: "kasir-5e3ad.appspot.com",
-  messagingSenderId: "1022262477243",
-  appId: "1:1022262477243:web:8637d3184d242c3af3049e"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
