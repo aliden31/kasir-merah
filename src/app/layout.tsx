1b273b2f4940eb56e761ad88e69cba971d53ed55
Nyai Tarsih <ntarsih32@gmail.com>
2025-08-01 10:53:18 +0000

    Ketika di double klik di layar jangan dibuat zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({
     <html lang="id">
       <head>
         <title>Toko Cepat</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
