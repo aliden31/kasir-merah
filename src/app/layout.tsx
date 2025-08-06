4821216a63b56d7a0dc53e7172e747570ee1d5ad
Nyai Tarsih <ntarsih32@gmail.com>
2025-08-06 19:34:54 +0000

    Apakah web ini bisa di jadikan app mobile. Dengan jsonnya

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,6 +72,8 @@ export default function RootLayout({
       <head>
         <title>Toko Cepat</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#f9faf7" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
