5ae191c1da5daf9d60800164ee40c47a5d7c3d82
Nyai Tarsih <ntarsih32@gmail.com>
2025-08-14 20:00:57 +0000

    Gunakan ai gemini-2.0-flash

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,7 +5,7 @@ export const ai = genkit({
   plugins: [googleAI({
     apiKey: process.env.GEMINI_API_KEY,
   })],
-  model: 'googleai/gemini-2.5-flash',
+  model: 'googleai/gemini-2.0-flash',
   config: {
     temperature: 0.1,
   },
