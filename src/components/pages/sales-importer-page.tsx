ae8dcaec37a915049f9e8ba475216f95bd13bdc3
Nyai Tarsih <ntarsih32@gmail.com>
2025-08-16 06:57:21 +0000

    Cek Error Tipe TypeScript

--- src/components/pages/sales-importer-page.tsx
+++ src/components/pages/sales-importer-page.tsx
@@ -644,7 +644,7 @@ const SalesImporterPage: React.FC<SalesImporterPageProps> = ({ onImportComplete,
             }
 
             const finalSales: Omit<Sale, 'id'>[] = salesToCreate.map(sale => {
-                const saleItems: SaleItem[] = sale.items.map((item: any) => {
+                const saleItems = sale.items.map((item: any) => {
                     let finalProductId: string | undefined;
                     let importSku = item.sku;
                     const existingProduct = dbProducts.find(p => p.id.toLowerCase() === importSku.toLowerCase());
@@ -988,3 +988,6 @@ const SalesImporterPage: React.FC<SalesImporterPageProps> = ({ onImportComplete,
 
 export default SalesImporterPage;
 
+
+
+    
